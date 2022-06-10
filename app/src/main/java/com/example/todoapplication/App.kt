3c331e1f7a09a331de9dd95dab4ce4362a51a86f
Chi Tran <chi.tran@veriksystems.com>
2022-06-10 07:58:00 +0700

    adding dependencies

---
+++ app/src/main/java/com/example/todoapplication/App.kt
@@ -0,0 +1,31 @@
+package com.example.todoapplication
+
+import android.app.Activity
+import android.app.Application
+import android.os.Bundle
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application(), Application.ActivityLifecycleCallbacks {
+    override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
+
+    }
+
+    override fun onActivityStarted(activity: Activity) {
+    }
+
+    override fun onActivityResumed(activity: Activity) {
+    }
+
+    override fun onActivityPaused(activity: Activity) {
+    }
+
+    override fun onActivityStopped(activity: Activity) {
+    }
+
+    override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
+    }
+
+    override fun onActivityDestroyed(activity: Activity) {
+    }
+}
